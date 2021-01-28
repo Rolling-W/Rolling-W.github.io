cdf6ff38334fcf394a7e411cfeeb06a4c6914f38
Rolling-W <40225724@qq.com>
2021-01-29 06:37:31 +0800

    Update project.js

--- src/project.js
+++ src/project.js
@@ -2038,7 +2038,7 @@ window.__require = function e(t, n, o) {
                     this.ajaxLoad("http://www.wesane.com/admin.php/Gamescore/saveGamescore", "gameScore=" + e + "&gameId=" + this.gameHttpId + "&gameType=" + t, this.scoreResult)
                 },
                 gamePV_load: function() {
-                    this.ajaxLoad("http://www.wesane.com/admin.php/Activityshow/gamelogo", "gameID=" + this.gameHttpId, this.ajaxOnLogoResult)
+                    this.ajaxLoad("http://www.wesane.com/", "gameID=" + this.gameHttpId, this.ajaxOnLogoResult)
                 },
                 ajaxOnLogoResult: function() {},
                 ajaxLoad: function(e, t, n) {
