8f73a24b438600c035a0a0c312a03763a596351a
Rolling-W <40225724@qq.com>
2021-05-01 18:13:08 +0800

    Update

--- js/love.js
+++ js/love.js
@@ -29,13 +29,14 @@ function detectBrowser()
         alert("请使用最新版本的谷歌浏览器以获得最佳体验。\n网页将随后关闭！");
         close();
     }
-    if(isMobile == false){
-        alert("请在电脑端使用最新版本的谷歌浏览器以获得最佳体验。\n网页将随后关闭！");
-        close();
-    }
+    // if(isMobile == false){
+        // alert("请在电脑端使用最新版本的谷歌浏览器以获得最佳体验。\n网页将随后关闭！");
+        // close();
+    // }
 }
 
 window.onload = function(){
+    var screenWidth = window.screen.width;
     detectBrowser()
     var max = 8, min = 2
     var ClickTimes = 0
@@ -45,7 +46,7 @@ window.onload = function(){
     insideL.onclick = function(){
         // alert("点击");
         ClickTimes = ClickTimes + 1;
-        if(ClickTimes == hastoTimes){
+        if(ClickTimes == hastoTimes && screenWidth >= 1080){
               
             document.getElementById("inside_love_h1").innerHTML = "❤";
             // document.getElementById("inside_love_h2").innerHTML = "占位符";
@@ -59,7 +60,26 @@ window.onload = function(){
             document.getElementById("inside_love_h1").style.fontWeight = 'normal';
             document.getElementById("inside_love").style.cursor = 'auto';
             document.getElementById("inside_p").innerHTML = " ";
-            alert("测试版本：\n本次点击的次数为 "+ ClickTimes);  
+            //alert("本次点击的次数为 "+ ClickTimes);  
+            alert("恭喜你发现彩蛋！"); 
+            // document.getElementById("inside_love_h1").style.lineHeight = '100px';
+        }
+        if(ClickTimes == hastoTimes && screenWidth < 1080){
+              
+            document.getElementById("inside_love_h1").innerHTML = "❤";
+            // document.getElementById("inside_love_h2").innerHTML = "占位符";
+            document.getElementById("inside_love_h1").style.fontSize = '250px';
+            document.getElementById("inside_love_h1").style.color='rgba(240, 97, 72, 0.55 )';
+            document.getElementById("inside_love_h1").style.width = 'auto';
+            document.getElementById("love").style.borderRadius = '0px';
+            document.getElementById("love").style.height = '100%';
+            document.getElementById("love").style.width = '100%';
+            document.getElementById("love").style.color = 'rgba(252, 166, 166, 0.295)';
+            document.getElementById("inside_love_h1").style.fontWeight = 'normal';
+            document.getElementById("inside_love").style.cursor = 'auto';
+            document.getElementById("inside_p").innerHTML = " ";
+            //alert("本次点击的次数为 "+ ClickTimes);  
+            alert("恭喜你发现彩蛋！"); 
             // document.getElementById("inside_love_h1").style.lineHeight = '100px';
         }
         
